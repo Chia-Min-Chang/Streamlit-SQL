dbd214d4b99b410c0fbe497b178812967259eb59
Chia-Min-Chang <125644332+Chia-Min-Chang@users.noreply.github.com>
2023-05-07 21:36:34 +0800

    Create app2.py

---
+++ app2.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import streamlit as st
+
+import altair as alt
+import duckdb
+
+con = duckdb.connect(database='job.db', read_only=True) 
+
+# Countries
+query="""
+   SELECT * 
+   FROM job
+"""
+Countries=list(con.execute(query).df().columns)[2:]
+
+
+st.subheader('Investigation')
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    query="""
+            SELECT 
+                 DISTINCT variable
+            From job        
+            ORDER BY variable       
+          """
+
+    kinds=con.execute(query).df()
+    kind = st.selectbox('Kind of Statistics',kinds)
+
+with col2: 
+    countries = st.multiselect('Countries',Countries)
+
+with col3:
+    result_df = con.execute(f"""
+        SELECT 
+            date, {','.join(countries)}
+        FROM Job 
+        WHERE variable=?
+    """, [kind]).df()
+
+    chart = alt.Chart(result_df).mark_line().encode(
+        x='date',
+        y=alt.Y(','.join(countries), axis=alt.Axis(title='Value'))
+    ).interactive()
+
+    st.altair_chart(chart, theme="streamlit", use_container_width=True)
